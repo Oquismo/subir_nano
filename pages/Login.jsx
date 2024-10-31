ca3fbfd36ec3373f9d20c21f4d1c9434ae49da5e
Stefano <161383899+Oquismo@users.noreply.github.com>
2024-10-31 04:52:15 +0100

    Add files via upload

---
+++ pages/Login.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import axios from 'axios'
+import { useRef, useState, useContext} from 'react'
+import { UsuarioContext } from '../contexto/UsuarioContext'
+import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+
+
+function Login() {
+
+const redireccionar = useNavigate()
+
+const { login} = useContext(UsuarioContext);
+
+const [error, setError] = useState('')
+
+const usuarioRef = useRef()
+
+const contrasenaRef = useRef()
+
+function checkearUsuario (e) {
+  e.preventDefault();
+
+  let objetoAMandar = {
+
+    usuario: usuarioRef.current.value,
+    contrasena: contrasenaRef.current.value
+
+  }
+
+  // http://localhost:3000/usuarios
+// axios.post('https://subir-nano-server.vercel.app/usuarios', objetoAMandar).then(datos=>{ IMPORTANTE CAMBIAR A SU SERVIDOR!!!})
+
+
+axios.post('http://localhost:3000/usuarios', objetoAMandar).then(datos=>{
+
+  if(datos.data.mensajeError == 'usuario no encontrado'){
+    setError('El usuario o la contraseña no son correctos')
+  }else{
+    login(datos.data.usuario)
+    redireccionar('/inicio')
+  }
+})
+}
+
+
+
+
+  return (
+    <>
+
+{/* <img  src={logo} className="logo" alt="Logo F1" /> */}
+    <h1 >Iniciar sesión</h1>
+    <form className='logStyle' action="#" method='post' onSubmit={checkearUsuario}>
+      <label htmlFor="usu"> Nombre de Usuario:  </label>
+      <input type="text" name='usuario' id='usu' ref={usuarioRef} /> <br />
+      <label htmlFor="pass">Contraseña: </label>
+      <input type="password" name='password' id='pass'  ref={contrasenaRef}/> <br />
+      <input type="submit" value=' iniciar Sesion ' /> <br />
+      {error}
+    </form>
+    <Link to={'/inicioInvitado'}>
+  <input className='InviteButton' type="submit" value=' Entrar como invitado' />
+</Link>
+    </>
+  )
+}
+
+export default Login
---
+++ pages/Merchant.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import { useContext } from "react";
+import { UsuarioContext } from '../contexto/UsuarioContext';
+import '../styles/merchan.css'
+import gorra  from '../img/GorraAlonso.jpg'
+import camisetaB from '../img/Camisetanormal.jpg'
+import camisetaA from '../img/CamisetaAlonso.jpg'
+import gorraRB from '../img/GorraRB.jpg'
+import camisetaRB from '../img/CamisetaRB.jpg'
+import sudadera from '../img/sudaderaRB.jpg'
+import Footer from '../components/Footer';
+
+
+
+function Merchant() {
+  const {usuario} = useContext(UsuarioContext)
+
+  return (
+    <>
+    <div className='Merch-Ini'>
+      <h1>Hola {usuario}, bienvenido a tu tienda oficial</h1>
+
+    </div>
+    <div className='product-section'>
+      <div className='product-flex'>
+        <div className='product-item'>
+          <h2 className='product-title'>
+            <img src={gorra} alt="" />
+          </h2>
+          <p className='product-description'>Gorra oficial del equipo Aston Martin</p>
+          <p className='product-price'>Precio: 20€</p>
+          <button className='product-button'>Compra</button>
+        </div>
+        <div className='product-item'>
+          <h2 className='product-title'>
+            <img src={camisetaB} alt="" />
+          </h2>
+          <p className='product-description'>Camistea oficial Aston Martin</p>
+          <p className='product-price'>Precio: 33€</p>
+          <button className='product-button'>Compra</button>
+        </div>
+        <div className='product-item'>
+          <h2 className='product-title'>
+            <img src={camisetaA} alt="" />
+          </h2>
+          <p className='product-description'>Gorra oficial Fernando Alonso</p>
+          <p className='product-price'>Precio: 45€</p>
+          <button className='product-button'>Compra</button>
+        </div>
+      </div>
+    </div>
+{/* tienda 2 */}
+    <div className='product-section'>
+      <div className='product-flex'>
+        <div className='product-itemRB'>
+          <h2 className='product-title'>
+            <img src={gorraRB} alt="" />
+          </h2>
+          <p className='product-description'>Gorra oficial edicion especial Red Bull</p>
+          <p className='product-price'>Precio: 20€</p>
+          <button className='product-button'>Compra</button>
+        </div>
+        <div className='product-itemRB'>
+          <h2 className='product-title'>
+            <img src={camisetaRB} alt="" />
+          </h2>
+          <p className='product-description'>Camistea oficial del equipo Red Bull</p>
+          <p className='product-price'>Precio: 33€</p>
+          <button className='product-button'>Compra</button>
+        </div>
+        <div className='product-itemRB'>
+          <h2 className='product-title'> 
+            <img src={sudadera} alt="" />
+          </h2>
+          <p className='product-description'>Sudadera oficial Red bull</p>
+          <p className='product-price'>Precio: 45€</p>
+          <button className='product-button'>Compra</button>
+        </div>
+      </div>
+    </div>
+<Footer/>
+    </>
+  )
+}
+
+export default Merchant
